72275bbcd8a6a5fe5509d14d2c8450c4e3ca4d93
anisul islam <anisulislam980340@gmail.com>
2024-02-16 19:31:21 +0600

    navlink completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
+  const NavbarStyles=({isActive})=>{
+    return {
+      fontWight: isActive ? "bold" : "normal",
+      textDecoration: isActive ? "none" : "underline",
+    }
+  }
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        <NavLink style={NavbarStyles} to="/">Home</NavLink>
+        <NavLink style={NavbarStyles} to="/about">About</NavLink>
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">About</Link> */}
     </nav>
   )
 }
